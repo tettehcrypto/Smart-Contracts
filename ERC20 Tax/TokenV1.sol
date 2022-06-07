ae2d7a13daa552ad56b35e21f5756852c686771f
Amaechi Okolobi <okolobiam@gmail.com>
2022-06-07 15:27:35 +0100

    TokenV1 update

--- ERC20 Tax/TokenV1.sol
+++ ERC20 Tax/TokenV1.sol
@@ -31,19 +31,21 @@ contract Token is ERC20, Ownable {
         string memory symbol,
         address _marketing,
         address _lpWallet,
-        address _lpPair,
         address _devWallet,
         uint8 _devTax,
         uint8 _marketingTax,
-        uint8 _liquidityTax
+        uint8 _liquidityTax,
+        uint16 _maxSell,
+        uint16 _maxBuy
     ) ERC20(name, symbol){
         marketingWallet = _marketing;
         lpWallet = _lpWallet;
-        lpPair = _lpPair;
         devWallet = _devWallet;
         devTax = _devTax;
         marketingTax = _marketingTax;
         liquidityTax = _liquidityTax;
+        maxSell = _maxSell;
+        maxBuy = _maxBuy;
     }
     
     function _transfer(
@@ -51,8 +53,8 @@ contract Token is ERC20, Ownable {
         address to,
         uint256 amount
     ) internal override virtual {
-        uint256 amountReceived = takeTaxes(from, to, amount);
-        uint256 taxAmount = amount - amountReceived;
+        uint256 taxAmount = takeTaxes(from, to, amount);
+        uint256 amountReceived = amount - taxAmount;
 
         require(distributeTax(from, taxAmount));
         super._transfer(from, to, amountReceived);
@@ -67,9 +69,9 @@ contract Token is ERC20, Ownable {
     }
 
     function distributeTax(address from, uint256 taxAmount) internal returns (bool) {
-        uint256 toMarketing = (taxAmount/marketingTax) * 100;
-        uint256 toDev = (taxAmount/devTax) * 100;
-        uint256 toLp = (taxAmount/liquidityTax) * 100;
+        uint256 toMarketing = (taxAmount/100)*marketingTax;
+        uint256 toDev = (taxAmount/100)*devTax;
+        uint256 toLp = taxAmount - toMarketing - toDev;
 
         super._transfer(from, marketingWallet, toMarketing);
         super._transfer(from, devWallet, toDev);
@@ -90,41 +92,19 @@ contract Token is ERC20, Ownable {
         if (from == lpPair /* address(this)*/) { // buying 
             require(buyCount + 1 <=  maxBuy, "Transfer Failed");
             currentFee = buyTax;
-            finalAmount = (amount/currentFee)/100;
+            finalAmount = (amount/100)*currentFee;
             buyCount++;
         } else if (to == lpPair /* address(this)*/) { //selling
             require(sellCount + 1 <= maxSell, "Transfer Failed");
             uint256 tokenBalance = balanceOf(from);
-            uint256 percentage = (amount/tokenBalance)/100;
+            uint256 percentage = (amount/tokenBalance)*100;
             currentFee = getTax(percentage);
-            finalAmount = (amount/currentFee)/100;
+            finalAmount = (amount/100)*currentFee;
             sellCount++;
         } else {
             return amount; 
         }
         return finalAmount;
-    }
-
-    function getTax(uint256 percentage) internal view returns (uint256) {
-        if (percentage < 20){
-            return sellTaxValues[0];
-        } else if (percentage >= 20 && percentage < 30){
-            return sellTaxValues[1];
-        } else if (percentage >= 30 && percentage < 40){
-            return sellTaxValues[2];
-        } else if (percentage >= 40 && percentage < 50){
-            return sellTaxValues[3];
-        } else if (percentage >= 50 && percentage < 60){
-            return sellTaxValues[4];
-        } else if (percentage >= 60 && percentage < 70){
-            return sellTaxValues[5];
-        } else if (percentage >= 70 && percentage < 80){
-            return sellTaxValues[6];
-        } else if (percentage >= 80 && percentage < 90){
-            return sellTaxValues[7];
-        } else  {
-            return sellTaxValues[8];
-        }   
     }
 
     /* Parameter Functions */
@@ -152,15 +132,40 @@ contract Token is ERC20, Ownable {
         maxSell = _maxSell;
     }
 
-    function resetSellCount(uint16 _sellCount) external onlyOwner {
+    function resetSellCount() external onlyOwner {
         sellCount =0;
     }
 
-    function resetBuyCount(uint16 _buyCount) external onlyOwner {
+    function resetBuyCount() external onlyOwner {
         buyCount =0;
     }
+
+    function setLpPair(address _lpPair) external onlyOwner {
+        lpPair = _lpPair;
+    }
+
     /* View / Pure Functions */
 
+    function getTax(uint256 percentage) internal view returns (uint256) {
+        if (percentage < 20){
+            return sellTaxValues[0];
+        } else if (percentage >= 20 && percentage < 30){
+            return sellTaxValues[1];
+        } else if (percentage >= 30 && percentage < 40){
+            return sellTaxValues[2];
+        } else if (percentage >= 40 && percentage < 50){
+            return sellTaxValues[3];
+        } else if (percentage >= 50 && percentage < 60){
+            return sellTaxValues[4];
+        } else if (percentage >= 60 && percentage < 70){
+            return sellTaxValues[5];
+        } else if (percentage >= 70 && percentage < 80){
+            return sellTaxValues[6];
+        } else if (percentage >= 80 && percentage < 90){
+            return sellTaxValues[7];
+        } else  {
+            return sellTaxValues[8];
+        }   
+    }
 
 }
-
